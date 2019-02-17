4b4c5f1400cee1feafed2c550651b62fff67d492
Dejan Babic <b.dejan96@outlook.com>
2019-02-17 01:03:22 +0100

    dodavanje rezultata

--- Rezultati/Controllers/UtakmicaController.cs
+++ Rezultati/Controllers/UtakmicaController.cs
@@ -81,6 +81,7 @@ namespace Rezultati.Controllers
                     TrecaCetvrtinaDomaci = 0,
                     TrecaCetvrtinaGost = 0
                 });
+                context.SaveChanges();
                 var utakmice = context.Utakmica.Where(u => u.DatumOdigravanja == datumUtakmice).Select(u => new UtakmicaViewModel
                 {
                     UtakmicaId = u.UtakmicaId,
@@ -102,9 +103,113 @@ namespace Rezultati.Controllers
                     PoeniGost = u.PrvaCetvrtinaGost + u.DrugaCetvrtinaGost + u.TrecaCetvrtinaGost + u.CetvrtaCetvrtinaGost
                 }).ToList();
                 return PartialView("_Utakmice", utakmice);
-;
+
             }
         }
+
+        public JsonResult Result(int id)
+        {
+            using (var context = new RezultatiEntities())
+            {
+                var sveUtakmice = context.Utakmica.Where(u => u.UtakmicaId == id).Select(u => new UtakmicaViewModel
+                {
+                    UtakmicaId = u.UtakmicaId,
+                    DatumOdigravanja = u.DatumOdigravanja,
+                    Kolo = u.Kolo,
+                    GostujuciTimId = u.GostujuciTimId,
+                    GostujuciTim = u.Tim1.Grad + " " + u.Tim1.Naziv,
+                    DomaciTimId = u.DomaciTimId,
+                    DomaciTim = u.Tim.Grad + " " + u.Tim.Naziv,
+                    PrvaCetvrtinaDomaci = u.PrvaCetvrtinaDomaci,
+                    DrugaCetvrtinaDomaci = u.DrugaCetvrtinaDomaci,
+                    TrecaCetvrtinaDomaci = u.TrecaCetvrtinaDomaci,
+                    CetvrtaCetvrtinaDomaci = u.CetvrtaCetvrtinaDomaci,
+                    PoeniDomaci = u.PrvaCetvrtinaDomaci + u.DrugaCetvrtinaDomaci + u.TrecaCetvrtinaDomaci + u.CetvrtaCetvrtinaDomaci,
+                    PrvaCetvrtinaGost = u.PrvaCetvrtinaGost,
+                    DrugaCetvrtinaGost = u.DrugaCetvrtinaGost,
+                    TrecaCetvrtinaGost = u.TrecaCetvrtinaGost,
+                    CetvrtaCetvrtinaGost = u.CetvrtaCetvrtinaGost,
+                    PoeniGost = u.PrvaCetvrtinaGost + u.DrugaCetvrtinaGost + u.TrecaCetvrtinaGost + u.CetvrtaCetvrtinaGost
+                }).ToList();
+
+                UtakmicaViewModel utakmica = sveUtakmice[0];
+
+                return new JsonResult() { Data = utakmica, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Result(int Id, int PrvaDomaci, int DrugaDomaci, int TrecaDomaci, int CetvrtaDomaci, int PrvaGost, int DrugaGost, int TrecaGost, int CetvrtaGost)
+        {
+            using (var context = new RezultatiEntities())
+            {
+                var rezultat = context.Utakmica.Find(Id);
+                rezultat.CetvrtaCetvrtinaDomaci = CetvrtaDomaci;
+                rezultat.CetvrtaCetvrtinaGost = CetvrtaGost;
+                rezultat.DrugaCetvrtinaDomaci = DrugaDomaci;
+                rezultat.DrugaCetvrtinaGost = DrugaGost;
+                rezultat.PrvaCetvrtinaDomaci = PrvaDomaci;
+                rezultat.PrvaCetvrtinaGost = PrvaGost;
+                rezultat.TrecaCetvrtinaDomaci = TrecaDomaci;
+                rezultat.TrecaCetvrtinaGost = TrecaGost;
+                context.SaveChanges();
+
+                DateTime datumUtakmice = rezultat.DatumOdigravanja;
+                var utakmice = context.Utakmica.Where(u => u.DatumOdigravanja == datumUtakmice).Select(u => new UtakmicaViewModel
+                {
+                    UtakmicaId = u.UtakmicaId,
+                    DatumOdigravanja = u.DatumOdigravanja,
+                    Kolo = u.Kolo,
+                    GostujuciTimId = u.GostujuciTimId,
+                    GostujuciTim = u.Tim1.Grad + " " + u.Tim1.Naziv,
+                    DomaciTimId = u.DomaciTimId,
+                    DomaciTim = u.Tim.Grad + " " + u.Tim.Naziv,
+                    PrvaCetvrtinaDomaci = u.PrvaCetvrtinaDomaci,
+                    DrugaCetvrtinaDomaci = u.DrugaCetvrtinaDomaci,
+                    TrecaCetvrtinaDomaci = u.TrecaCetvrtinaDomaci,
+                    CetvrtaCetvrtinaDomaci = u.CetvrtaCetvrtinaDomaci,
+                    PoeniDomaci = u.PrvaCetvrtinaDomaci + u.DrugaCetvrtinaDomaci + u.TrecaCetvrtinaDomaci + u.CetvrtaCetvrtinaDomaci,
+                    PrvaCetvrtinaGost = u.PrvaCetvrtinaGost,
+                    DrugaCetvrtinaGost = u.DrugaCetvrtinaGost,
+                    TrecaCetvrtinaGost = u.TrecaCetvrtinaGost,
+                    CetvrtaCetvrtinaGost = u.CetvrtaCetvrtinaGost,
+                    PoeniGost = u.PrvaCetvrtinaGost + u.DrugaCetvrtinaGost + u.TrecaCetvrtinaGost + u.CetvrtaCetvrtinaGost
+                }).ToList();
+                return PartialView("_Utakmice", utakmice);
+
+            }
+        }
+
+        public ActionResult ChangeDate(string datum)
+        {
+            using (var context = new RezultatiEntities())
+            {
+                DateTime dtm = Convert.ToDateTime(datum).Date;
+                var utakmice = context.Utakmica.Where(u => u.DatumOdigravanja == dtm).Select(u => new UtakmicaViewModel
+                {
+                    UtakmicaId = u.UtakmicaId,
+                    DatumOdigravanja = u.DatumOdigravanja,
+                    Kolo = u.Kolo,
+                    GostujuciTimId = u.GostujuciTimId,
+                    GostujuciTim = u.Tim1.Grad + " " + u.Tim1.Naziv,
+                    DomaciTimId = u.DomaciTimId,
+                    DomaciTim = u.Tim.Grad + " " + u.Tim.Naziv,
+                    PrvaCetvrtinaDomaci = u.PrvaCetvrtinaDomaci,
+                    DrugaCetvrtinaDomaci = u.DrugaCetvrtinaDomaci,
+                    TrecaCetvrtinaDomaci = u.TrecaCetvrtinaDomaci,
+                    CetvrtaCetvrtinaDomaci = u.CetvrtaCetvrtinaDomaci,
+                    PoeniDomaci = u.PrvaCetvrtinaDomaci + u.DrugaCetvrtinaDomaci + u.TrecaCetvrtinaDomaci + u.CetvrtaCetvrtinaDomaci,
+                    PrvaCetvrtinaGost = u.PrvaCetvrtinaGost,
+                    DrugaCetvrtinaGost = u.DrugaCetvrtinaGost,
+                    TrecaCetvrtinaGost = u.TrecaCetvrtinaGost,
+                    CetvrtaCetvrtinaGost = u.CetvrtaCetvrtinaGost,
+                    PoeniGost = u.PrvaCetvrtinaGost + u.DrugaCetvrtinaGost + u.TrecaCetvrtinaGost + u.CetvrtaCetvrtinaGost
+                }).ToList();
+                return PartialView("_Utakmice", utakmice);
+            }
+        }
+
 
     }
 }
---
+++ Rezultati/Models/UcinakViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rezultati.Models
+{
+    public class UcinakViewModel
+    {
+        public int UcinakId { get; set; }
+        public int UtakmicaId { get; set; }
+        public int IgracId { get; set; }
+        public string ImeIgraca { get; set; }
+        public string Tim { get; set; }
+        public short BrojMinuta { get; set; }
+        public short BrojPoena { get; set; }
+        public short BrojAsistencija { get; set; }
+        public short BrojSkokova { get; set; }
+        public short PokusajiIzIgre { get; set; }
+        public short PogodjeniSutevi { get; set; }
+        public short SlobodnaBacanja { get; set; }
+        public short PogodjenaBacanja { get; set; }
+        public short LicneGreske { get; set; }
+        public short SuteviZaTri { get; set; }
+        public short PogodjeniZaTri { get; set; }
+        public short UkradeneLopte { get; set; }
+        public short Blokovi { get; set; }
+        public short IzgubljeneLopte { get; set; }
+    }
+}
